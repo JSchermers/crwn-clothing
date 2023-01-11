d7a8f545312e305e6110b9216c409aa718c883c6
Joost <joost.schermers@protonmail.com>
2023-01-11 21:15:24 +0100

    update routing

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,12 +1,16 @@
 import "./category-item.styles.scss"
+import { useNavigate } from "react-router-dom";
 
 
 const CategoryItem = ({category}) => {
-    const {id, imageUrl, title} = category;
+    const {id, imageUrl, title, route} = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route)
 
     return  <div key={id} className="category-container">
     <div style={{backgroundImage: `url(${imageUrl})`}} className={"background-image"}></div>
-    <div className="category-body-container">
+    <div className="category-body-container" onClick={onNavigateHandler}>
       <h2>{title}</h2>
       <p>shop now</p>
     </div>  
